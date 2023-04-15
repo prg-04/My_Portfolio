79df11bf2ac910d777be23dcc2961200926415fa
prg-04 <karanjaevans95@gmail.com>
2023-04-15 18:03:54 +0300

    Add scroll animation

--- index.js
+++ index.js
@@ -13,3 +13,29 @@ links.forEach((link) => {
         navlinks.classList.remove('open')
     })
 })
+
+if ($(".text-slider").length == 1) {
+             
+            var typed_strings =
+                $(".text-slider-items").text();
+ 
+            var typed = new Typed(".text-slider", {
+                strings: typed_strings.split(", "),
+                typeSpeed: 50,
+                loop: false,
+                backDelay: 100,
+                backSpeed: 30,
+            });
+}
+        
+$(window).scroll(function() {
+    $('.animate-on-scroll').each(function() {
+        var position = $(this).offset().top;
+        var scrollPosition = $(window).scrollTop();
+        if (position < scrollPosition + $(window).height()) {
+            $(this).addClass('animated');
+        }
+    });
+});
+
+AOS.init();
