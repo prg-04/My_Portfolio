e93e6c2e2fc31d352017355d1430b3759e16680b
prg-04 <karanjaevans95@gmail.com>
2023-04-20 18:19:35 +0300

    Add eslint test file and fix linters error

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const toggle = document.querySelector(".toggle");
+const toggle = document.querySelector('.toggle');
 const navlinks = document.querySelector('.navlinks');
-const links = document.querySelectorAll('.link')
+const links = document.querySelectorAll('.link');
 
-toggle.addEventListener("click", () => {
-    toggle.classList.toggle("open");
-    navlinks.classList.toggle('open')
+toggle.addEventListener('click', () => {
+  toggle.classList.toggle('open');
+  navlinks.classList.toggle('open');
 });
 
-
 links.forEach((link) => {
-    link.addEventListener('click', () => {
-        toggle.classList.remove('open')
-        navlinks.classList.remove('open')
-    })
-})
+  link.addEventListener('click', () => {
+    toggle.classList.remove('open');
+    navlinks.classList.remove('open');
+  });
+});
