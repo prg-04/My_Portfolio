0e5d836e2ba80e3d5540ccd07080f1994d5174c5
prg-04 <karanjaevans95@gmail.com>
2023-04-20 11:01:07 +0300

    Add popup modal

--- constants/constants.js
+++ constants/constants.js
@@ -4,57 +4,76 @@ export const cardsData = [
     placeHolder: [
       {
         image: "./assets/Img_Placeholder.png",
-        alt: 'yoga',
+        alt: "yoga",
         title: "Multi-Post Stories",
         description:
           "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
         technologies: ["css", "HTML", "Boostrap", "Ruby"],
-        button: 'See Project'
+        button: "See Project",
       },
     ],
     cards: [
       {
-        title: ['Professional Art', "Printing Data"],
+        title: ["Professional Art", "Printing Data"],
         description:
           "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
         technologies: ["html", "bootstrap", "Ruby"],
-         button: 'See Project'
+        button: "See Project",
       },
       {
-        title: ['Professional Art', "Printing Data"],
+        title: ["Professional Art", "Printing Data"],
         description:
           "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
         technologies: ["html", "bootstrap", "Ruby"],
-         button: 'See Project'
+        button: "See Project",
       },
       {
-        title: ['Professional Art', "Printing Data"],
+        title: ["Professional Art", "Printing Data"],
         description:
           "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
         technologies: ["html", "bootstrap", "Ruby"],
-         button: 'See Project'
+        button: "See Project",
       },
       {
-        title: ['Professional Art', "Printing Data"],
+        title: ["Professional Art", "Printing Data"],
         description:
           "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
         technologies: ["html", "bootstrap", "Ruby"],
-         button: 'See Project'
+        button: "See Project",
       },
       {
-        title: ['Professional Art', "Printing Data"],
+        title: ["Professional Art", "Printing Data"],
         description:
           "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
         technologies: ["html", "bootstrap", "Ruby"],
-         button: 'See Project'
+        button: "See Project",
       },
       {
-        title: ['Professional Art', "Printing Data"],
+        title: ["Professional Art", "Printing Data"],
         description:
           "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
         technologies: ["html", "bootstrap", "Ruby"],
-         button: 'See Project'
+        button: "See Project",
       },
     ],
+  },
+];
+
+export const modalData = [
+  {
+    title: [
+      "Multi Post Stories",
+      "Keeping track of hundreds of components website",
+    ],
+    technologies: ["html", "bootstrap", "Ruby on rails"],
+    images: [
+      "./assets/modal_desk.png",
+      "./assets/modal_mob.png",
+      "./assets/Icon.png",
+    ],
+    text: " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been  industry's standard dummy text since the 1500s, when an unknown printer took a galley of type and  scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    text2:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, ",
+    buttons: ["See Live", "See Source"],
   },
 ];
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { cardsData } from "./constants/constants.js";
+import { cardsData, modalData } from "./constants/constants.js";
 const toggle = document.querySelector(".toggle");
 const navlinks = document.querySelector(".navlinks");
 const links = document.querySelectorAll(".link");
@@ -14,12 +14,13 @@ links.forEach((link) => {
     navlinks.classList.remove("open");
   });
 });
-
 if ($(".text-slider").length == 1) {
-  var typed_strings = $(".text-slider-items").text();
+  var typed_strings = $(".text-slider-items").html();
+
+  typed_strings = typed_strings.replace(/,\s/g, "<br />");
 
   var typed = new Typed(".text-slider", {
-    strings: typed_strings.split(", "),
+    strings: typed_strings.split("<br />"),
     typeSpeed: 50,
     loop: false,
     backDelay: 100,
@@ -36,6 +37,8 @@ $(window).scroll(function () {
     }
   });
 });
+
+
 
 const { title, placeHolder, cards } = cardsData[0];
 
@@ -57,6 +60,7 @@ portfolio.appendChild(titleDiv);
 
 const cardsDiv = makeDiv();
 cardsDiv.className = "cards";
+cardsDiv.classList.add("blur");
 
 cardsDiv.innerHTML = cardsData.map((item) => {
   const { placeHolder, cards } = item;
@@ -71,11 +75,11 @@ cardsDiv.innerHTML = cardsData.map((item) => {
     button: btn,
   } = cards[0];
 
-  return `<div class="placeholder">
-          <div class="plchdr_img" data-aos="fade-down-right">
+  return `<div class="placeholder wow fadeInDown">
+          <div class="plchdr_img">
             <img src=${image} alt=${alt} />
           </div>
-          <div class="plchdr_text" data-aos="fade-down-left">
+          <div class="plchdr_text fadeInDown">
             <h3>${title}</h3>
             <p class="description">
               ${description}
@@ -86,10 +90,11 @@ cardsDiv.innerHTML = cardsData.map((item) => {
               <li>${bootstrap}</li>
               <li>${ruby}</li>
             </ul>
-            <button type="button">${button}</button>
+            <button type="button" class="bttn">${button}</button>
           </div>
         </div>
-        <div class="card card_1" data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine">
+       <div class="card card_1" data-scrollmagic="fade-right" data-scrollmagic-offset="300" data-scrollmagic-easing="ease-in-sine" data-scrollmagic-id="card_1">
+
           <h3 class="card_title">
             ${title2[0]}<br />
             ${title2[1]}
@@ -102,9 +107,10 @@ cardsDiv.innerHTML = cardsData.map((item) => {
             <li class="tech">${bootstrap}</li>
             <li class="tech">${ruby}</li>
           </ul>
-          <button class="card_btn" type="button">${btn}</button>
+          <button class="card_btn bttn" type="button">${btn}</button>
         </div>
-        <div class="card card_2" data-aos="fade-right" data-aos-offset="400" data-aos-easing="ease-in-sine">
+        <div class="card card_2" data-scrollmagic="fade-right" data-scrollmagic-offset="400" data-scrollmagic-easing="ease-in-sine" data-scrollmagic-id="card_2">
+
           <h3 class="card_title">
             ${title2[0]}<br />
             ${title2[1]}
@@ -117,9 +123,10 @@ cardsDiv.innerHTML = cardsData.map((item) => {
             <li class="tech">${bootstrap}</li>
             <li class="tech">${ruby}</li>
           </ul>
-          <button class="card_btn" type="button">${btn}</button>
+          <button class="card_btn bttn" type="button">${btn}</button>
         </div>
-        <div class="card card_3" data-aos="fade-right" data-aos-offset="500" data-aos-easing="ease-in-sine">
+        <div class="card card_3" data-scrollmagic="fade-right" data-scrollmagic-offset="500" data-scrollmagic-easing="ease-in-sine" data-scrollmagic-id="card_3">
+
           <h3 class="card_title">
             ${title2[0]}<br />
             ${title2[1]}
@@ -132,9 +139,10 @@ cardsDiv.innerHTML = cardsData.map((item) => {
             <li class="tech">${bootstrap}</li>
             <li class="tech">${ruby}</li>
           </ul>
-          <button class="card_btn" type="button">${btn}</button>
-        </div>
-        <div class="card card_4" data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine">
+          <button class="card_btn bttn" type="button">${btn}</button>
+          </div>
+          <div class="card card_4" data-scrollmagic="fade-right" data-scrollmagic-offset="300" data-scrollmagic-easing="ease-in-sine" data-scrollmagic-id="card_4">
+
           <h3 class="card_title">
             ${title2[0]}<br />
             ${title2[1]}
@@ -147,9 +155,10 @@ cardsDiv.innerHTML = cardsData.map((item) => {
             <li class="tech">${bootstrap}</li>
             <li class="tech">${ruby}</li>
           </ul>
-          <button class="card_btn" type="button">${btn}</button>
+          <button class="card_btn bttn" type="button">${btn}</button>
         </div>
-        <div class="card card_5" data-aos="fade-right" data-aos-offset="400" data-aos-easing="ease-in-sine">
+        <div class="card card_5" data-scrollmagic="fade-right" data-scrollmagic-offset="400" data-scrollmagic-easing="ease-in-sine" data-scrollmagic-id="card_5">
+
           <h3 class="card_title">
             ${title2[0]}<br />
             ${title2[1]}
@@ -162,10 +171,11 @@ cardsDiv.innerHTML = cardsData.map((item) => {
             <li class="tech">${bootstrap}</li>
             <li class="tech">${ruby}</li>
           </ul>
-          <button class="card_btn" type="button">${btn}</button>
+          <button class="card_btn bttn" type="button">${btn}</button>
         </div>
-        <div class="card card_6" data-aos="fade-right" data-aos-offset="500" data-aos-easing="ease-in-sine">
-         <h3 class="card_title">
+        <div class="card card_6" data-scrollmagic="fade-right" data-scrollmagic-offset="500" data-scrollmagic-easing="ease-in-sine" data-scrollmagic-id="card_6">
+
+          <h3 class="card_title">
             ${title2[0]}<br />
             ${title2[1]}
           </h3>
@@ -177,10 +187,179 @@ cardsDiv.innerHTML = cardsData.map((item) => {
             <li class="tech">${bootstrap}</li>
             <li class="tech">${ruby}</li>
           </ul>
-          <button class="card_btn" type="button">${btn}</button>
-        </div>`;
+          <button class="card_btn bttn" type="button">${btn}</button>
+          </div>
+          `;
 });
 
 portfolio.appendChild(cardsDiv);
 
-AOS.init();
+const popup = makeDiv();
+popup.className = "modal";
+popup.innerHTML = modalData.map((item) => {
+  const { title, technologies, images, text, text2, buttons } = item;
+  const [title1, title2] = title;
+  const [html, bootstrap, ruby] = technologies;
+  const [img1, img2, icon] = images;
+  const [btn1, btn2] = buttons;
+  return ` <div class="heading">
+          <div class="pop-title">
+            <h3 class="modal_ttl title1">
+              ${title2}
+            </h3>
+            <h3 class="modal_ttl title2">${title1}</h3>
+            <i class="fa-regular fa-x cls-modal"></i>
+          </div>
+          <ul class="modal_tech">
+            <li class="md_tech">${html}</li>
+            <li class="md_tech">${bootstrap}</li>
+            <li class="md_tech">${ruby}</li>
+          </ul>
+        </div>
+        <div class="content">
+          <div class="image">
+            <img
+              class="pop_desk"
+              src=${img1}
+              alt="portfolio"
+            />
+            <img class="pop_mob" src=${img2} alt="portfolio" />
+          </div>
+          <div class="description">
+            <p class="modal_text">
+              ${text}
+            </p>
+            <p class="modal_text md-txt">
+              ${text2} 
+            </p>
+            <div class="modal_buttons">
+              <button class="btn" type="button">
+               ${btn1}<span><img src=${icon} alt="icon" /></span>
+              </button>
+              <button class="btn" type="button">
+               ${btn2}<span><i class="fa-brands fa-github"></i></span>
+              </button>
+            </div>
+          </div>
+        </div>`;
+});
+
+portfolio.appendChild(popup);
+
+// Add code here
+
+const aboutMe = document.querySelector(".about_me");
+new ScrollMagic.Scene({
+  triggerElement: aboutMe,
+  triggerHook: 0.8,
+})
+  .setTween(
+    TweenMax.fromTo(
+      aboutMe,
+      1,
+      {
+        opacity: 0,
+        y: 20,
+      },
+      {
+        opacity: 1,
+        y: 0,
+        ease: "ease-in",
+      }
+    )
+  )
+  .addTo(controller);
+
+const skillStack1 = document.querySelector(".stack-one");
+new ScrollMagic.Scene({
+  triggerElement: skillStack1,
+  triggerHook: 0.8,
+  offset: 300,
+})
+  .setTween(
+    TweenMax.fromTo(
+      skillStack1,
+      1,
+      {
+        opacity: 0,
+        x: -20,
+      },
+      {
+        opacity: 1,
+        x: 0,
+        ease: "ease-in",
+      }
+    )
+  )
+  .addTo(controller);
+
+const skillStack2 = document.querySelector(".skill_stack:nth-child(2)");
+new ScrollMagic.Scene({
+  triggerElement: skillStack2,
+  triggerHook: 0.8,
+  offset: 400,
+})
+  .setTween(
+    TweenMax.fromTo(
+      skillStack2,
+      1,
+      {
+        opacity: 0,
+        x: -20,
+      },
+      {
+        opacity: 1,
+        x: 0,
+        ease: "ease-in",
+      }
+    )
+  )
+  .addTo(controller);
+
+const skillStack3 = document.querySelector(".skill_stack:nth-child(3)");
+new ScrollMagic.Scene({
+  triggerElement: skillStack3,
+  triggerHook: 0.8,
+  offset: 500,
+})
+  .setTween(
+    TweenMax.fromTo(
+      skillStack3,
+      1,
+      {
+        opacity: 0,
+        x: -20,
+      },
+      {
+        opacity: 1,
+        x: -15,
+        ease: "ease-in",
+      }
+    )
+  )
+  .addTo(controller);
+
+const btns = document.querySelectorAll(".bttn");
+const modal = document.querySelector(".modal");
+const clsModal = document.querySelector(".cls-modal");
+const blur = document.querySelectorAll(".blur");
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    modal.classList.add("open");
+
+    blur.forEach((blurElement) => {
+      blurElement.classList.add("open");
+    });
+  });
+});
+
+clsModal.addEventListener("click", () => {
+  modal.classList.remove("open");
+
+  blur.forEach((blurElement) => {
+    blurElement.classList.remove("open");
+  });
+});
+
+
