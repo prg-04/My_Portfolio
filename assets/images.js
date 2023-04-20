73bcb5ce878a7c4e67332756a20545297711f85c
prg-04 <karanjaevans95@gmail.com>
2023-04-20 15:34:55 +0300

    reinstall eslint and fix linter errors

--- assets/images.js
+++ assets/images.js
@@ -1,4 +1,4 @@
-export { default as Img_Placeholder } from './Img_Placeholder.png';
-export { default as card1 } from './card-1.png'
-export { default as card2 } from './card_2.png'
-export { default as card3 } from './card_3.png'
+export { default as ImgPlaceholder } from './Img_Placeholder.png';
+export { default as card1 } from './card-1.png';
+export { default as card2 } from './card_2.png';
+export { default as card3 } from './card_3.png';
--- index.js
+++ index.js
@@ -1,26 +1,29 @@
-import { cardsData, modalData } from "./constants/constants.js";
-const toggle = document.querySelector(".toggle");
-const navlinks = document.querySelector(".navlinks");
-const links = document.querySelectorAll(".link");
+import Typed from 'typed.js';
+import gsap from 'gsap';
+import ScrollMagic from 'scrollmagic';
+import { cardsData, modalData } from './constants/constants.js';
 
-toggle.addEventListener("click", () => {
-  toggle.classList.toggle("open");
-  navlinks.classList.toggle("open");
+const toggle = document.querySelector('.toggle');
+const navlinks = document.querySelector('.navlinks');
+const links = document.querySelectorAll('.link');
+
+toggle.addEventListener('click', () => {
+  toggle.classList.toggle('open');
+  navlinks.classList.toggle('open');
 });
 
 links.forEach((link) => {
-  link.addEventListener("click", () => {
-    toggle.classList.remove("open");
-    navlinks.classList.remove("open");
+  link.addEventListener('click', () => {
+    toggle.classList.remove('open');
+    navlinks.classList.remove('open');
   });
 });
-if ($(".text-slider").length == 1) {
-  var typed_strings = $(".text-slider-items").html();
-
-  typed_strings = typed_strings.replace(/,\s/g, "<br />");
-
-  var typed = new Typed(".text-slider", {
-    strings: typed_strings.split("<br />"),
+if ('jQuery'('.text-slider').length === 1) {
+  let typedStrings = 'jQuery'('.text-slider-items').html();
+  typedStrings = typedStrings.replace(/,\s/g, '<br />');
+  // eslint-disable-next-line no-unused-vars
+  const typed = new Typed('.text-slider', {
+    strings: typedStrings.split('<br />'),
     typeSpeed: 50,
     loop: false,
     backDelay: 100,
@@ -28,50 +31,44 @@ if ($(".text-slider").length == 1) {
   });
 }
 
-$(window).scroll(function () {
-  $(".animate-on-scroll").each(function () {
-    var position = $(this).offset().top;
-    var scrollPosition = $(window).scrollTop();
-    if (position < scrollPosition + $(window).height()) {
-      $(this).addClass("animated");
+'jQuery'(window).scroll(() => {
+  'jQuery'('.animate-on-scroll').each(function scrll() {
+    const position = 'jQuery'(this).offset().top;
+    const scrollPosition = 'jQuery'(window).scrollTop();
+    if (position < scrollPosition + 'jQuery'(window).height()) {
+      'jQuery'(this).addClass('animated');
     }
   });
 });
 
-const { title, placeHolder, cards } = cardsData[0];
-
-const portfolio = document.getElementById("portfolio");
-
+const { title } = cardsData[0];
+const portfolio = document.getElementById('portfolio');
 function makeDiv() {
-  const div = document.createElement("div");
+  const div = document.createElement('div');
   return div;
 }
 
 const titleDiv = makeDiv();
-titleDiv.className = "title";
+titleDiv.className = 'title';
 titleDiv.innerHTML = `<h2>${title}</h2>`;
 
-const hr = document.createElement("hr");
-hr.className = "line";
+const hr = document.createElement('hr');
+hr.className = 'line';
 titleDiv.appendChild(hr);
 portfolio.appendChild(titleDiv);
 
 const cardsDiv = makeDiv();
-cardsDiv.className = "cards";
-cardsDiv.classList.add("blur");
+cardsDiv.className = 'cards';
+cardsDiv.classList.add('blur');
 
 cardsDiv.innerHTML = cardsData.map((item) => {
   const { placeHolder, cards } = item;
-  const { image, alt, title, description, button, technologies } =
-    placeHolder[0];
+  const {
+    image, alt, title, description, button, technologies,
+  } = placeHolder[0];
   const [css, html, bootstrap, ruby] = technologies;
 
-  const {
-    title: title2,
-    description: description2,
-    technologies: tech,
-    button: btn,
-  } = cards[0];
+  const { title: title2, description: description2, button: btn } = cards[0];
 
   return `<div class="placeholder wow fadeInDown">
           <div class="plchdr_img">
@@ -193,9 +190,11 @@ cardsDiv.innerHTML = cardsData.map((item) => {
 portfolio.appendChild(cardsDiv);
 
 const popup = makeDiv();
-popup.className = "modal";
+popup.className = 'modal';
 popup.innerHTML = modalData.map((item) => {
-  const { title, technologies, images, text, text2, buttons } = item;
+  const {
+    title, technologies, images, text, text2, buttons,
+  } = item;
   const [title1, title2] = title;
   const [html, bootstrap, ruby] = technologies;
   const [img1, img2, icon] = images;
@@ -245,26 +244,27 @@ popup.innerHTML = modalData.map((item) => {
 portfolio.appendChild(popup);
 
 const controller = new ScrollMagic.Controller();
-document.querySelectorAll(".card").forEach((card) => {
-  const animation = card.getAttribute("data-scrollmagic");
-  const offset = parseInt(card.getAttribute("data-scrollmagic-offset"));
-  const easing = card.getAttribute("data-scrollmagic-easing");
+
+document.querySelectorAll('.card').forEach((card) => {
+  const animation = card.getAttribute('data-scrollmagic');
+  const offset = parseInt(card.getAttribute('data-scrollmagic-offset'), 10);
+  const easing = card.getAttribute('data-scrollmagic-easing');
   new ScrollMagic.Scene({
     triggerElement: card,
     triggerHook: 0.8,
-    offset: offset,
+    offset,
     reverse: true,
   })
     .setClassToggle(card, animation)
     .addTo(controller)
-    .on("enter", (event) => {
+    .on('enter', () => {
       card.classList.add(animation);
     })
-    .on("leave", (event) => {
+    .on('leave', () => {
       card.classList.remove(animation);
     })
     .setTween(
-      TweenMax.fromTo(
+      gsap.fromTo(
         card,
         1,
         {
@@ -275,18 +275,18 @@ document.querySelectorAll(".card").forEach((card) => {
           opacity: 1,
           y: 0,
           ease: easing,
-        }
-      )
+        },
+      ),
     );
 });
 
-const aboutMe = document.querySelector(".about_me");
+const aboutMe = document.querySelector('.about_me');
 new ScrollMagic.Scene({
   triggerElement: aboutMe,
   triggerHook: 0.8,
 })
   .setTween(
-    TweenMax.fromTo(
+    gsap.fromTo(
       aboutMe,
       1,
       {
@@ -296,20 +296,20 @@ new ScrollMagic.Scene({
       {
         opacity: 1,
         y: 0,
-        ease: "ease-in",
-      }
-    )
+        ease: 'ease-in',
+      },
+    ),
   )
   .addTo(controller);
 
-const skillStack1 = document.querySelector(".stack-one");
+const skillStack1 = document.querySelector('.stack-one');
 new ScrollMagic.Scene({
   triggerElement: skillStack1,
   triggerHook: 0.8,
   offset: 300,
 })
   .setTween(
-    TweenMax.fromTo(
+    gsap.fromTo(
       skillStack1,
       1,
       {
@@ -319,20 +319,20 @@ new ScrollMagic.Scene({
       {
         opacity: 1,
         x: 0,
-        ease: "ease-in",
-      }
-    )
+        ease: 'ease-in',
+      },
+    ),
   )
   .addTo(controller);
 
-const skillStack2 = document.querySelector(".skill_stack:nth-child(2)");
+const skillStack2 = document.querySelector('.skill_stack:nth-child(2)');
 new ScrollMagic.Scene({
   triggerElement: skillStack2,
   triggerHook: 0.8,
   offset: 400,
 })
   .setTween(
-    TweenMax.fromTo(
+    gsap.fromTo(
       skillStack2,
       1,
       {
@@ -342,20 +342,20 @@ new ScrollMagic.Scene({
       {
         opacity: 1,
         x: 0,
-        ease: "ease-in",
-      }
-    )
+        ease: 'ease-in',
+      },
+    ),
   )
   .addTo(controller);
 
-const skillStack3 = document.querySelector(".skill_stack:nth-child(3)");
+const skillStack3 = document.querySelector('.skill_stack:nth-child(3)');
 new ScrollMagic.Scene({
   triggerElement: skillStack3,
   triggerHook: 0.8,
   offset: 500,
 })
   .setTween(
-    TweenMax.fromTo(
+    gsap.fromTo(
       skillStack3,
       1,
       {
@@ -365,31 +365,31 @@ new ScrollMagic.Scene({
       {
         opacity: 1,
         x: -15,
-        ease: "ease-in",
-      }
-    )
+        ease: 'ease-in',
+      },
+    ),
   )
   .addTo(controller);
 
-const btns = document.querySelectorAll(".bttn");
-const modal = document.querySelector(".modal");
-const clsModal = document.querySelector(".cls-modal");
-const blur = document.querySelectorAll(".blur");
+const btns = document.querySelectorAll('.bttn');
+const modal = document.querySelector('.modal');
+const clsModal = document.querySelector('.cls-modal');
+const blur = document.querySelectorAll('.blur');
 
 btns.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    modal.classList.add("open");
+  btn.addEventListener('click', () => {
+    modal.classList.add('open');
 
     blur.forEach((blurElement) => {
-      blurElement.classList.add("open");
+      blurElement.classList.add('open');
     });
   });
 });
 
-clsModal.addEventListener("click", () => {
-  modal.classList.remove("open");
+clsModal.addEventListener('click', () => {
+  modal.classList.remove('open');
 
   blur.forEach((blurElement) => {
-    blurElement.classList.remove("open");
+    blurElement.classList.remove('open');
   });
 });
